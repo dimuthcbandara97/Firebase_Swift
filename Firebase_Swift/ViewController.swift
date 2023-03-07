4bdc43e008995ddfe9e6823e9041557c6519a581
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-07 13:58:32 +0530

    Initial Commit

---
+++ Firebase_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Swift
+//
+//  Created by Dimuth Bandara on 2023-03-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
