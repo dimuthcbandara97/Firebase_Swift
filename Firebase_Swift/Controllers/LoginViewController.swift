6dac1f6b8c1944301e22da55b4a1405fe0292e6c
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-03-07 16:42:22 +0530

    Custom Buttons added and Login View basic UI completed

--- Firebase_Swift/Controllers/LoginViewController.swift
+++ Firebase_Swift/Controllers/LoginViewController.swift
@@ -12,22 +12,31 @@ import UIKit
 class LoginViewController: UIViewController {
     
     // MARK: - UI Components
+    
+    // header view
     private let headerView = AuthHeaderView(title: "Sign In", subTitle: "Sign in to your account")
     
+    // user name and password
     private let usernameField = CustomTextField(fieldType: .username)
     private let passwordField = CustomTextField(fieldType: .password)
     
-    // Define buttons
-    
-    //
+    // Buttons
+    private let signInButton = CustomButton(title: "Sign In", hasBackground: true, fontSize: .big)
+    private let newUserButton = CustomButton(title: "New User? Create Account.", fontSize: .med)
+    private let forgotPasswordButton = CustomButton(title: "Forgot Password?", fontSize: .small)
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        // UI setup
         self.setupUI()
         
-        // setting up buttons
-        
+        // Buttons
+        self.signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        self.newUserButton.addTarget(self, action: #selector(didTapNewUser), for: .touchUpInside)
+        self.forgotPasswordButton.addTarget(self, action: #selector(didTapForgotPassword), for: .touchUpInside)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,37 +48,63 @@ class LoginViewController: UIViewController {
     private func setupUI() {
         self.view.backgroundColor = .systemBackground
         
+        // Adding Sub Views
         self.view.addSubview(headerView)
         self.view.addSubview(usernameField)
         self.view.addSubview(passwordField)
+        self.view.addSubview(signInButton)
+        self.view.addSubview(newUserButton)
+        self.view.addSubview(forgotPasswordButton)
         
-        // Add button subviews
         
-        
+        // setting translatesAutoresizingMaskIntoConstraints
         headerView.translatesAutoresizingMaskIntoConstraints = false
         usernameField.translatesAutoresizingMaskIntoConstraints = false
         passwordField.translatesAutoresizingMaskIntoConstraints = false
-       
-        // Add button contraints
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        newUserButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
 
+        
+        // Setting constraints
         NSLayoutConstraint.activate([
+            // header View
             self.headerView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
             self.headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             self.headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             self.headerView.heightAnchor.constraint(equalToConstant: 222),
             
+            // User name
             self.usernameField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 12),
             self.usernameField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
             self.usernameField.heightAnchor.constraint(equalToConstant: 55),
             self.usernameField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
             
+            // Password
             self.passwordField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 22),
             self.passwordField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
             self.passwordField.heightAnchor.constraint(equalToConstant: 55),
             self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
             
+            // Sign In Button
+            self.signInButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 22),
+            self.signInButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.signInButton.heightAnchor.constraint(equalToConstant: 55),
+            self.signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
             
-            // button constraints here
+            
+            // New User Button
+            self.newUserButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 11),
+            self.newUserButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.newUserButton.heightAnchor.constraint(equalToConstant: 44),
+            self.newUserButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            
+            // Forget password button
+            self.forgotPasswordButton.topAnchor.constraint(equalTo: newUserButton.bottomAnchor, constant: 6),
+            self.forgotPasswordButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.forgotPasswordButton.heightAnchor.constraint(equalToConstant: 44),
+            self.forgotPasswordButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
         ])
     }
     
